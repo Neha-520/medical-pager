2ffcde54832e79c5096a7519c2757083e9f14515
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-12-18 21:11:23 +0530

    installed dependencies

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Cookies from 'universal-cookie';
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+
+const apiKey = '6hpumqmubw58'
+
+function App() {
+  return (
+    <div className="App">
+       <Chat>
+         
+       </Chat> 
+    </div>
+  );
+}
+
+export default App;
